2994350f17ad8533f8dbcb9b98a05a7981018ad3
josh <wei080781@gmail.com>
2026-03-18 16:48:07 +0800

    更新專案規劃書與移除登入頁面

--- pet_shop/resources/views/welcome.blade.php
+++ pet_shop/resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <div class="flex items-center space-x-5">
             <!-- 1. 搜尋圖標 (放大鏡) -->
             <button class="text-stone-500 hover:text-orange-400 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" fil="none" viewBox="0 0 24 24" stroke-width="1.5"
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m21
                 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196
